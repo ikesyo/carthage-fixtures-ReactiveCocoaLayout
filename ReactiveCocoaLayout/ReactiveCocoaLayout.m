2c4fb49d23f13ed863228beb64c9f9397e300ce2
Sho Ikeda <suicaicoca@gmail.com>
2017-07-30 15:37:58 +0900

    Make ReactiveCocoaLayout a dummy framework

---
+++ ReactiveCocoaLayout/ReactiveCocoaLayout.m
@@ -0,0 +1,13 @@
+//
+//  ReactiveCocoaLayout.m
+//  ReactiveCocoaLayout
+//
+//  Created by Justin Spahr-Summers on 2014-11-25.
+//  Copyright (c) 2014 GitHub. All rights reserved.
+//
+
+#import "ReactiveCocoaLayout.h"
+
+@implementation ReactiveCocoaLayout
+
+@end
