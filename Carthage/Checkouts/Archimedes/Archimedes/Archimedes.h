aaf9fa02e7f42fdf98545026652178f3363d520a
Sho Ikeda <suicaicoca@gmail.com>
2017-07-30 23:10:03 +0900

    Make Archimedes a dummy framework

--- Carthage/Checkouts/Archimedes/Archimedes/Archimedes.h
+++ Carthage/Checkouts/Archimedes/Archimedes/Archimedes.h
@@ -2,19 +2,12 @@
 //  Archimedes.h
 //  Archimedes
 //
-//  Created by Justin Spahr-Summers on 2014-10-02.
+//  Created by Justin Spahr-Summers on 2014-11-25.
 //  Copyright (c) 2014 GitHub. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-#import <CoreGraphics/CoreGraphics.h>
 
-//! Project version number for Archimedes.
-FOUNDATION_EXPORT double ArchimedesVersionNumber;
+@interface Archimedes : NSObject
 
-//! Project version string for Archimedes.
-FOUNDATION_EXPORT const unsigned char ArchimedesVersionString[];
-
-#import <Archimedes/CGGeometry+MEDConvenienceAdditions.h>
-#import <Archimedes/NSValue+MEDGeometryAdditions.h>
-#import <Archimedes/MEDEdgeInsets.h>
+@end
---
+++ Carthage/Checkouts/Archimedes/Archimedes/Archimedes.m
@@ -0,0 +1,13 @@
+//
+//  Archimedes.m
+//  Archimedes
+//
+//  Created by Justin Spahr-Summers on 2014-11-25.
+//  Copyright (c) 2014 GitHub. All rights reserved.
+//
+
+#import "Archimedes.h"
+
+@implementation Archimedes
+
+@end
